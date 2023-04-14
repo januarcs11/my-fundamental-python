135eae1f04d5d2576d124e6187fcdbbf0a10d4f7
Achmad Januar <achmad.januar@smartfren.com>
2023-04-14 10:43:50 +0700

    sequential

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 """
-ini project pertama
+Semua sintaksis dasar bahasa pemograman terdiri dari
+1. sequential : Langkah berurutan
+2. Percabangan : Langkah melompat jika kondisi terpenuhi
+3. Perulangan : mengulang langkah yang sama berkali kalis selama semua kondisi terpenuhi
 """
 
-print("Hello brohhh")
-print("My Name is januar")
+# SEQUNETIAL
+print('Ibu Berkata", "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko"')
+print('Ibu Menjawab, "Beli satu botol susu, dan jika ada telor beli 6"')
+print("Maka budi berangkat ke toko")
+print("dan mulai belanja")
+
+# Percabangan
+
