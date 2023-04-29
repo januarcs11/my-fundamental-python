70c04522f56d3e66cd26cf7a69b7108767e1ceac
januarcs11 <januar.oss@gmail.com>
2023-04-29 22:00:56 +0700

    reduce variable name

---
+++ Perulangan_while_undefined_count.py
@@ -0,0 +1,25 @@
+"""
+Program perulangan dengan while
+"""
+
+buku = 10
+print("Ibu Berkata baca semua buku mu")
+jumlah_baca = 0
+
+jumlah_pahami = 0
+print(f'Jumlah buku yang sudah di baca dan di pahami {jumlah_pahami}')
+
+
+while jumlah_baca < buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_pahami == 9:
+        print(f"Buku ke {jumlah_pahami + 1} belum paham ")
+    else:
+        jumlah_pahami = jumlah_pahami + 1
+        print(f"Buku ke {jumlah_pahami} sudah dibaca dan di pahami")
+print(f'Jumlah buku yang sudah dibaca dan dipahami {jumlah_pahami}')
+if jumlah_pahami == buku:
+    print('"Bum semua buka sudah dibaca dan di pahami')
+else:
+    print(f'"Bu , tidak semua buka bisa di pahami. '
+          f'budi hanya memahami {jumlah_pahami} buku')
